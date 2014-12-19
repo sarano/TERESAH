3b02e5baa2eea9599ed30f15e0432e1437900f23
sarano <silvia.arano@upf.edu>
2014-12-19 09:48:55 +0100

    Update views.php

--- app/lang/es/views.php
+++ app/lang/es/views.php
@@ -4,7 +4,7 @@ return array(
     "admin" => array(
         "activities" => array(
             "index" => array(
-                "heading" => "Recent activity",
+                "heading" => "Actividad reciente",
                 "listing_results" => "Listing recent activity from <span class=\"label round\">:from</span> to <span class=\"label round\">:to</span> of <span class=\"label round\">:total</span> available."
             ),
 
